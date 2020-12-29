a2a03f49073eb64692e697a04b9fdcb75cce0bde
Esteban Alvarado <estalvgs1999@estudiantec.cr>
2020-12-29 16:00:48 -0600

    feat: player and team models done

---
+++ coach-board/src/app/models/player.model.ts
@@ -0,0 +1,21 @@
+export interface Player {
+  $key?: string;
+  name: string;
+  lastName: string;
+  position: Positions;
+  weight: number;
+  height: number;
+  country: string; // Country Code ISO 3166-alpha-3
+  team: string;
+  jersey: string;
+}
+
+export enum Positions {
+  F = "Forward",
+  G = "Guard",
+  C = "Center",
+  PG = "Point Guard",
+  SG = "Shooting Guard",
+  SF = "Small Forward",
+  PF = "Power Forward"
+}
