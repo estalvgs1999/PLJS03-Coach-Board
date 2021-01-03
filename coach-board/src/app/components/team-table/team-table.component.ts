f0fc8e4f23d1ebd9cc3f23a29ac97b5024b48254
Esteban Alvarado <estalvgs1999@estudiantec.cr>
2021-01-02 21:58:35 -0600

    build (project structure): teams table

--- coach-board/src/app/components/team-table/team-table.component.ts
+++ coach-board/src/app/components/team-table/team-table.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TeamService } from '../../services/team.service';
+import { Team } from '../../models/team.model';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-team-table',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamTableComponent implements OnInit {
 
-  constructor() { }
+  public teams$: Observable<Team[]>;
 
+  constructor(private teamService: TeamService) { }
+
+  // Rigth place to make requests
   ngOnInit(): void {
+    this.teams$ = this.teamService.getTeams();
+    this.teamService.getTeams().pipe(take(1)).subscribe(teams => {
+      if (teams.length === 0) {
+        const team: Team = {
+          name: 'All Stars Team',
+          logoUrl: '/coach-board/src/assets/img.png',
+          players: []
+        };
+      }
+    });
   }
 
 }
